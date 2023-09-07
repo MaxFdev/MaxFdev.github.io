78cd3c8c18a666aa4f0f03c0d19925f3e703cb03
MaxFdev <mifrank1@mail.yu.edu>
2023-09-07 14:46:10 -0400

    Broke Menu
    Need to fix scrolling calculation and setting active link.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,50 @@
 
 // TODO: Make this track with scrolling
 
+// Function for getting active link ID
+function getActiveLinkId() {
+    let activeLinkId = null;
+    links.forEach(link => {
+        const linkTargetId = link.getAttribute('href').substring(1);
+        const linkSection = document.getElementById(linkTargetId);
+        const linkPosition = linkSection.offsetTop;
+        const linkBottomPosition = linkPosition + linkSection.offsetHeight;
+
+        if (window.scrollY >= linkPosition - 80 && window.scrollY < linkBottomPosition) {
+            activeLinkId = linkTargetId;
+        }
+    });
+    return activeLinkId;
+}
+
+// Function to update the "active" class for menu items based on scroll position
+function updateMenuActiveState(targetId) {
+    targetId.classList.add('active');
+    links.forEach(link => {
+        const linkTargetId = link.getAttribute('href').substring(1);
+        if (targetId !== linkTargetId) {
+            link.classList.remove('active');
+        }
+
+        // const linkSection = document.getElementById(linkTargetId);
+        // const linkPosition = linkSection.offsetTop;
+        // const linkBottomPosition = linkPosition + linkSection.offsetHeight;
+
+        // // Check for intro and contact
+        // if (linkTargetId === 'hero' || linkTargetId === 'contact') {
+        //     if (window.scrollY >= linkPosition - 400 && window.scrollY < linkBottomPosition) {
+        //         link.classList.add('active');
+        //     } else {
+        //         link.classList.remove('active');
+        //     }
+        // // Check if the scroll position is within the bounds of this link's section
+        // } else if (window.scrollY >= linkPosition - 80 && window.scrollY < linkBottomPosition) {
+        //     link.classList.add('active');
+        // } else {
+        // }
+    });
+}
+
 // Function to handle smooth scrolling when a link is clicked
 function scrollToSection(event) {
     event.preventDefault(); // Prevent the default link behavior
@@ -24,19 +68,26 @@ function scrollToSection(event) {
         });
     }
 
-    // Remove the "active" class from all links
-    links.forEach(link => {
-        link.classList.remove('active');
-    });
+    updateMenuActiveState(targetId);
+    // // Remove the "active" class from all links
+    // links.forEach(link => {
+    //     link.classList.remove('active');
+    // });
 
-    // Add the "active" class to the clicked link
-    this.classList.add('active');
+    // // Add the "active" class to the clicked link
+    // this.classList.add('active');
 }
 
 // Attach the scrollToSection function to all links with class "scroll-link"
 const links = document.querySelectorAll('a[href^="#"]');
 links.forEach(link => {
     link.addEventListener('click', scrollToSection);
+});
+
+// Attach an event listener to update the "active" class on scroll
+window.addEventListener('scroll', function() {
+    const activeLinkId = getActiveLinkId();
+    updateMenuActiveState(activeLinkId);
 });
 
 /*
